12d957a9e17c9cbcd8af29897452f450b1971b10
Tim <kunets.tim@gmail.com>
2020-05-05 13:20:44 +0300

    dockerfile

--- lib/settings.py
+++ lib/settings.py
@@ -1,6 +1,6 @@
 ############## TELEGRAM BOT ########################
 token = "1109533224:AAE13znQf9kZqTvGlIXPa8IbuO0zO6-yho8"
 ############## MSQL DATABASES ######################
-host = "13.48.24.227"
+host = "13.53.163.8"
 user = "root"
 passwd = "my-secret-pass"
