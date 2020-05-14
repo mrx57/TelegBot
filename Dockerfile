1a3e115e3b52b375bddba72442112c16fa1f3bf2
Tim <kunets.tim@gmail.com>
2020-05-14 13:06:21 +0300

    Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,9 @@
 FROM python:3
 WORKDIR /usr/src/app
+ENV DB_HOST=localhost
+ENV DB_USER=root
+ENV DB_PASSWORD=root
+ENV TELEGRAM_TOKEN=00000000000000000000000
 COPY . /usr/src/app
 RUN pip install --no-cache-dir -r requirements.txt
 CMD [ "python", "./start.py" ]
--- lib/settings.py
+++ lib/settings.py
@@ -1,6 +1,6 @@
 ############## TELEGRAM BOT ########################
 token = "1109533224:AAE13znQf9kZqTvGlIXPa8IbuO0zO6-yho8"
 ############## MSQL DATABASES ######################
-host = "13.53.163.8"
+host = "13.53.137.81"
 user = "root"
 passwd = "my-secret-pass"
